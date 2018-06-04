aa6e448abe52144a8e9a7850a38d83f3629ea19e
coralia <coralia.glisca@gmail.com>
2018-06-05 02:40:53 +0300

    update

--- Function.c
+++ Function.c
@@ -1,1 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+#include"function.h"
 
+extern int chromatic_no;
+extern int no_nodes;
+extern int counter_for_solution;
+extern int adjacency_matrix[100][100];
+extern int color[100];
+
+void NextValue( int k_node){
+	  int j;
+
+    while(1)
+    {
+
+        color[k_node]=(color[k_node]+1)%(chromatic_no+1);
+
+        for(j=1; j<=no_nodes; j++)
+        {
+                  if(adjacency_matrix[k_node][j]==1&&color[k_node]==color[j])
+                           break;
+        }
+
+    }
+
+}
+
+void GraphColoring(int k_node)  //generates a combination of colors and prints it
+{
+
+    int i;
+
+    while(1)
+    {
+
+        nextValue(k_node);
+
+        if(k_node==no_nodes)
+        {
+
+            counter_for_solution=1;
+
+            for(i=1; i<=no_nodes; i++)
+            {
+
+                printf("%d ", color[i]);
+            }
+
+
+            printf("\n");
+
+        }
+        else
+
+            GraphColoring(k_node+1);
+
+
+    }
+
+}
--- main.c
+++ main.c
@@ -2,7 +2,75 @@
 #include <stdlib.h>
 #include "Function.h"
 
-int main()
-{
+int chromatic_no;
+int  no_nodes;
+int counter_for_solution=0;
+int adjacency_matrix[100][100];
+int color[100]; //memoreaza culoarea nodului respectiv
+
+void  main(){
+        int i, j;
+
+        int existence_of_edge;
+
+       printf("\nEnter the number of nodes: ");
+
+       scanf("%d", &no_nodes);
+
+
+      printf("\n===Enter 1 if the nodes are adjacent, otherwise enter 0===\n");
+
+      for(i=1; i<=no_nodes; i++){
+
+                color[i]=0; //we've not colored it yet
+
+                 for(j=1; j<=no_nodes; j++) {
+
+                            if(i==j)
+
+                                    adjacency_matrix[i][j]=0;
+
+                             else{
+
+                                    printf("%d -> %d: " , i, j);
+                                    scanf("%d", &existence_of_edge);
+
+                                   adjacency_matrix[i][j]=adjacency_matrix[j][i]=existence_of_edge;
+
+                            }
+
+                     }
+
+           }
+
+
+       printf("\nEnter Adjacency Matrix:\n");
+
+       for(i=1;i<=no_nodes;i++){
+
+                for(j=1;j<=no_nodes;j++){
+
+                          printf("a[%d][%d]= ", i,j);
+                          scanf("%d", &adjacency_matrix[i][j]);
+                 }
+         }
+
+
+printf("\nPossible Solutions are\n");
+
+  for(chromatic_no=1;chromatic_no<=no_nodes;chromatic_no++){
+
+    if(counter_for_solution==1){
+
+       break;
+
+    }
+
+    GraphColoring(1);
+
+  }
+
+
+  printf("\nThe chromatic number is %d", chromatic_no);
 
 }
